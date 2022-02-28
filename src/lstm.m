67a7fd1af5112b486c4ce1257203ce27575a9294
Dera Uchenwoke <uchidera1@gmail.com>
2022-02-28 16:17:58 +0000

    Plotted trend, seasonal and irregular component of time series

--- src/lstm.m
+++ src/lstm.m
@@ -1,4 +1,4 @@
-%% Stage 0: Re-format Kegworth time series 
+%% Stage 0: Re-format original Kegworth time series 
 
 % Start time series after 7 year gap.
 completed_keg = kegworth(4474:15280, :); 
@@ -24,20 +24,36 @@ tmaxVals=rowfun(@max,final_keg,'InputVariables','Flows', ...
 
 % Plot maximum 7 day total flow against months forming new time series.
 figure
-subplot(3,1,1); 
+subplot(4,1,1); 
 plot(1:355, tmaxVals.GroupMax); 
 xlabel('Months');
-ylabel('Q* (m^3/s)');
+ylabel('Q (m^3/s)');
 xlim([0 355]);
 
-%% Stage 1: Conduct test for stationarity 
+%% Stage 1: Conduct Leybourne-McCabe test for stationarity 
 
-% Determine trend: Plot long-term trend on graph (Using trenddecomp MATLAB)
-trend = trenddecomp(tmaxVals.GroupMax); 
-subplot(3,1,2);
+[trend, seasonal, irregular] = trenddecomp(tmaxVals.GroupMax, ...
+    NumSeasonal=2); % 2 seasonal period specified to see...
+% short-term and long-term seasonality.
+% Plot long-term trend component: 
+subplot(4,1,2);
 plot(1:355, trend);
 xlabel('Months');
-ylabel('Q* (m^3/s)');
+ylabel('Trend');
+xlim([0 355]);
+
+% Plot seasonal component:
+subplot(4, 1, 3);
+plot(1:355, seasonal); 
+xlabel('Months');
+ylabel('Seasonal');
+xlim([0 355]);
+
+% Plot irregular component: 
+subplot(4, 1, 4);
+plot(1:355, irregular); 
+xlabel('Months');
+ylabel('Irregular');
 xlim([0 355]);
 
 % Determine optimal lag number via BIC methodology.
@@ -51,7 +67,6 @@ xlim([0 355]);
 
 %% Stage 2b: Pre-process data for ARIMA
 % Do (+PACF?)
-
-
+% When in doubt - Fetch -> Pull -> Push 
 
 
