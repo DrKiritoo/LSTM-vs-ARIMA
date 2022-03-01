65cc60e9148fa045d23544ad47bf7bf94bc7c32c
Dera Uchenwoke <uchidera1@gmail.com>
2022-03-01 18:07:05 +0000

    Conducted LM test, plotted ACF and PACF up to 20 lags

--- src/lstm.m
+++ src/lstm.m
@@ -25,7 +25,7 @@ tmaxVals=rowfun(@max,final_keg,'InputVariables','Flows', ...
 % Plot maximum 7 day total flow against months forming new time series.
 figure
 subplot(4,1,1); 
-plot(1:355, tmaxVals.GroupMax); 
+plot(1:355, tmaxVals.GroupMax, 'black'); 
 xlabel('Months');
 ylabel('Q (m^3/s)');
 xlim([0 355]);
@@ -37,31 +37,74 @@ xlim([0 355]);
 % short-term and long-term seasonality.
 % Plot long-term trend component: 
 subplot(4,1,2);
-plot(1:355, trend);
+plot(1:355, trend,'black');
 xlabel('Months');
-ylabel('Trend');
+ylabel('T_t');
 xlim([0 355]);
 
 % Plot seasonal component:
 subplot(4, 1, 3);
-plot(1:355, seasonal); 
+plot(1:355, seasonal,'black'); 
 xlabel('Months');
-ylabel('Seasonal');
+ylabel('S_t');
 xlim([0 355]);
 
 % Plot irregular component: 
 subplot(4, 1, 4);
-plot(1:355, irregular); 
+plot(1:355, irregular,'black'); 
 xlabel('Months');
-ylabel('Irregular');
+ylabel('I_t');
 xlim([0 355]);
 
 % Determine optimal lag number via BIC methodology.
+% (1) Create zeros matrix to store all 16 combinations of ARIMA models...
+LogL = zeros(4,4); 
+PQ = zeros(4,4);
+for p = 1:4
+    for q = 1:4
+        Mdl = arima(p, 0, q); % d = 0, since time series will not...
+        % undergo time series transformation via differencing.
+        [EstMdl,~,LogL(p, q)] = estimate(Mdl, tmaxVals.GroupMax, ...
+            'Display', 'off');
+        PQ(p, q) = p + q;
+     end
+end
 
+% (2) Calculate BIC per combination: 
+logL = LogL(:);
+pq = PQ(:);
+[~,bic] = aicbic(logL, pq + 1, 355);
+BIC = reshape(bic, 4, 4); 
 
-% Input final parameters into modified LM test. 
+% (3) Obtain the optimal number of lags by finding minimum BIC value:
+minBIC = min(BIC,[],'all');
+minP = find(minBIC == BIC); 
 
+% (3.1) Check estimated optimal model with ACF and PACF. 
+auto = autocorr(tmaxVals.GroupMax); 
+parauto = parcorr(tmaxVals.GroupMax);
+lags = (0:1:20); 
 
+figure 
+subplot(2, 1, 1); 
+plot(lags, auto, 'black'); 
+xlabel('lag {\tau}'); 
+ylabel('R ({\tau})');
+xlim([0 20]);
+
+subplot(2, 1, 2);
+plot(lags, parauto, 'black'); 
+xlabel('lag {\tau}'); 
+ylabel('R_{partial} ({\tau})');
+xlim([0 20]); 
+
+% (4) Input 'minP' into modified LM test to check for stationarity
+% If h = 0 then accept the null hypothesis. If h = 1 reject null.
+h = lmctest(tmaxVals.GroupMax,...
+    "trend", false, "Lags", 3, "Test", "var2", "alpha", 0.05);
+disp(h); 
+% In this case h = 0 therefore time series is stationary.
+% no additional stationary pre-processing is required.
 
 %% Stage 2a: Pre-process data for LSTM-RNN 
 
